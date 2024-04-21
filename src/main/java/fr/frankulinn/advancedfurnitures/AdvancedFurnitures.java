5cfa6df34361666ebd1f2c56637f5353d90dd833
Louis PAQUEREAU <paquereaulouis79300@gmail.com>
2024-04-21 14:03:06 +0200

    Created projects files

---
+++ src/main/java/fr/frankulinn/advancedfurnitures/AdvancedFurnitures.java
@@ -0,0 +1,17 @@
+package fr.frankulinn.advancedfurnitures;
+
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.logging.Logger;
+
+@Mod(AdvancedFurnitures.MOD_ID)
+public class AdvancedFurnitures {
+
+    public static final String MOD_ID = "advancedfurnitures";
+    public static final Logger LOGGER = Logger.getLogger("advancedfurnitures");
+
+    public AdvancedFurnitures() {
+
+
+    }
+}
